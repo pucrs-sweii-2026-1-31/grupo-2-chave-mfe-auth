2f64a0f9eb821e46d2d3097c42c3a361c8cb8c84
enrico-zini <enricoassmannzini@gmail.com>
2026-05-11 21:59:26 -0300

    chore: change name -> username on register payload

--- src/pages/RegisterPanel.tsx
+++ src/pages/RegisterPanel.tsx
@@ -92,7 +92,7 @@ export default function RegisterPanel({
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          name: nome,
+          username: nome,
           email,
           password: senha,
           role: perfil,
